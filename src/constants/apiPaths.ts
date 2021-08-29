d4fd1b426e5766c9abfaf101133f04b8381d7a94
Sergey Labetik <sergey.labetik@gmail.com>
2021-08-29 17:53:39 -0500

    Use Product service API, prettier

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://n4w7ktp7nk.execute-api.us-east-1.amazonaws.com/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
