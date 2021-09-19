df494f687f48eb0ede8392b17feae903d45ec199
Sergey Labetik <sergey.labetik@gmail.com>
2021-09-19 14:46:37 -0500

    Update API paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
-
 const API_PATHS = {
-  product: 'https://n4w7ktp7nk.execute-api.us-east-1.amazonaws.com/products',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://n4w7ktp7nk.execute-api.us-east-1.amazonaws.com/products",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://6wpg017ij5.execute-api.us-east-1.amazonaws.com/dev/thumbnails",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
