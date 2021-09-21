4719f6f164170f50ef5d23a3323c24ee6a37ac7a
Sergey Labetik <sergey.labetik@gmail.com>
2021-09-20 19:18:10 -0500

    fix apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://n4w7ktp7nk.execute-api.us-east-1.amazonaws.com/products",
+  product: "https://n4w7ktp7nk.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://6wpg017ij5.execute-api.us-east-1.amazonaws.com/dev/thumbnails",
+  import: "https://uy15iku6zi.execute-api.us-east-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
