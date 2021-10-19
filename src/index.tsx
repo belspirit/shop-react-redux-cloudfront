45485ae74d2ea03e1043f90fa530e97b3404f1b5
Sergey Labetik <sergey.labetik@gmail.com>
2021-10-18 20:54:09 -0500

    add alert messages for auth problems

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import axios from 'axios';
+import axios from "axios";
+import App from "components/App/App";
+import "index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "store/store";
+import * as serviceWorker from "./serviceWorker";
 
 axios.interceptors.response.use(
   response => {
     return response;
   },
-  function(error) {
-    if (error?.response?.status === 400) {
+  function (error) {
+    const responseStatus = error?.response?.status;
+
+    if (responseStatus === 400) {
       alert(error.response.data?.data);
+    }
+
+    if (responseStatus === 403 || responseStatus === 401) {
+      alert(error.response.data?.message);
     }
 
     return Promise.reject(error?.response ?? error);
@@ -24,11 +30,11 @@ axios.interceptors.response.use(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
