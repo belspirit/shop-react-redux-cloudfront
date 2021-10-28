ba034210bb712fb8cbe2a877799d7d5f53165143
Sergey Labetik <sergey.labetik@gmail.com>
2021-10-28 01:46:35 -0500

    fix: apiPaths.ts

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://yzurk88fkb.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://belspirit-cart-api-dev.us-east-1.elasticbeanstalk.com/api",
+  cart: "https://3glaghfyo8.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
